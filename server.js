f371a6af09d67eb37d882c54b487ede6dfbae3c9
Reinharty1 <dr.reinny@gmail.com>
2025-10-23 07:46:16 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -55,4 +55,7 @@ app.get("/leaderboard", (req, res) => {
 
 // 7️⃣ Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
